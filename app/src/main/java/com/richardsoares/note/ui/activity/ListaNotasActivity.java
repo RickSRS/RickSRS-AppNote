6dd8e92cf3757044a526bb7d971c334223848620
Richard Soares <soaresr870@gmail.com>
2022-08-09 22:21:14 -0300

    created activity to show notes

---
+++ app/src/main/java/com/richardsoares/note/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,15 @@
+package com.richardsoares.note.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.richardsoares.note.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
