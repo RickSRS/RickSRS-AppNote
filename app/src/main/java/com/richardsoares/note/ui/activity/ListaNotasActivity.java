32fcb55589051d47b5638a1e90229c1965d0071b
Richard Soares <soaresr870@gmail.com>
2022-08-09 22:44:19 -0300

    added default classes

--- app/src/main/java/com/richardsoares/note/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/richardsoares/note/ui/activity/ListaNotasActivity.java
@@ -2,8 +2,15 @@ package com.richardsoares.note.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.richardsoares.note.R;
+import com.richardsoares.note.dao.NotaDAO;
+import com.richardsoares.note.model.Nota;
+import com.richardsoares.note.ui.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -11,5 +18,12 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.lista_notas_listview);
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira Nota", "Primeira Descrição"));
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 }
