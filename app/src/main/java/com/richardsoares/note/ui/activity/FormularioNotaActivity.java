68ebe39ce3b550171ad96549f005207dd7e129c8
Richard Soares <soaresr870@gmail.com>
2022-08-13 22:43:58 -0300

    added button for save note

--- app/src/main/java/com/richardsoares/note/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/com/richardsoares/note/ui/activity/FormularioNotaActivity.java
@@ -3,6 +3,7 @@ package com.richardsoares.note.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.richardsoares.note.R;
 
@@ -12,5 +13,11 @@ public class FormularioNotaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nota);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario_salva_nota, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
