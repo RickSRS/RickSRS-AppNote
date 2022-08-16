b38f62fd61a3641682edc7316971a143f19e7bf6
Richard Soares <soaresr870@gmail.com>
2022-08-15 21:50:35 -0300

    remove data in the note to the swiped

--- app/src/main/java/com/richardsoares/note/ui/recycler/helper/callback/NotaItemTouchHelperCallback.java
+++ app/src/main/java/com/richardsoares/note/ui/recycler/helper/callback/NotaItemTouchHelperCallback.java
@@ -4,7 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.richardsoares.note.dao.NotaDAO;
+import com.richardsoares.note.ui.recycler.adapter.ListaNotasAdapter;
+
 public class NotaItemTouchHelperCallback extends ItemTouchHelper.Callback {
+
+    private ListaNotasAdapter adapter;
+
+    public NotaItemTouchHelperCallback(ListaNotasAdapter adapter) {
+        this.adapter = adapter;
+    }
+
     @Override
     public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
         int marcacoesDeDeslize = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
@@ -18,6 +28,8 @@ public class NotaItemTouchHelperCallback extends ItemTouchHelper.Callback {
 
     @Override
     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-
+        int indexNotaDeslize = viewHolder.getAdapterPosition();
+        new NotaDAO().remove(indexNotaDeslize);
+        adapter.remove(indexNotaDeslize);
     }
 }
